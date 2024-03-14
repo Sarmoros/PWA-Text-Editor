be5d377b83726b1195194d627ce71cd919b96e6c
Sarahy <sarahym0703@gmail.com>
2024-03-14 11:27:14 -0400

    added a click event handler on the 'butInstall' element

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -10,7 +10,19 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
+    if (deferredPrompt){
+        deferredPrompt.prompt();
+        const { outcome } = await deferredPrompt.userChoice;
+        if (outcome === 'accepted') {
+            console.log('PWA setup accepted');
+        } else {
+            console.log('PWA setup rejected');
+        }
+        deferredPrompt = null;
+        butInstall.style.display = 'none';
+    }
 });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+});
